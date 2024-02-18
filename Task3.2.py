7a612773996ab2cc2f68d4ebfa12c56135b97f12
Bronivick <mister.galiakhmetov2016@yandex.ru>
2024-02-18 20:46:00 +0300

    Task3.2

---
+++ Task3.2.py
@@ -0,0 +1,9 @@
+list_1 = [1, 2, 3, 4, 5]
+k = int(input("Введите число от 0 до 6: "))
+i = 0
+while i <= len(list_1):
+    if k <= list_1(i):
+        Number = list_1(i)
+    i += 1
+print(k)
+print(Number)
